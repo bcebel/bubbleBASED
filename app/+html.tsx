ac805a86a9616d6de8e1fbae07d655582497de51
bcebel <bcebel@gmail.com>
2026-09-22 01:48:10 -0500

    photos

--- app/+html.tsx
+++ app/+html.tsx
@@ -137,24 +137,10 @@ export default function Root({ children }: PropsWithChildren) {
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <link rel="icon" href="/48.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/16.png" />
 
         {/* Unified WebTorrent Initialization */}
         <script
@@ -268,7 +254,6 @@ export default function Root({ children }: PropsWithChildren) {
             zIndex: 99999,
           }}
         >
-
           <h1
             style={{
               fontSize: "2.5rem",
