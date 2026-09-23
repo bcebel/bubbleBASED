a5a8ff38f56bdb7901c86c0486fee46c7f2142bd
bcebel <bcebel@gmail.com>
2026-09-23 16:16:53 -0500

    turn

--- app/+html.tsx
+++ app/+html.tsx
@@ -172,13 +172,31 @@ window.WebTorrent = WebTorrent;
             tracker: {
               announce: window.enhancedTrackers,
               rtcConfig: {
-                iceServers: [
-                  { urls: "stun:stun.relay.metered.ca:80" },
-                  {
-                    urls: "turn:global.relay.metered.ca:80",
-                    username: "fe67734f65cabae0c1f0bf61",
-                    credential: "AY3FDMwL9QjEIZ2R",
-                  },
+    iceServers: [
+      {
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:standard.relay.metered.ca:80",
+        username: "fe67734f65cabae0c1f0bf61",
+        credential: "AY3FDMwL9QjEIZ2R",
+      },
+      {
+        urls: "turn:standard.relay.metered.ca:80?transport=tcp",
+        username: "fe67734f65cabae0c1f0bf61",
+        credential: "AY3FDMwL9QjEIZ2R",
+      },
+      {
+        urls: "turn:standard.relay.metered.ca:443",
+        username: "fe67734f65cabae0c1f0bf61",
+        credential: "AY3FDMwL9QjEIZ2R",
+      },
+      {
+        urls: "turns:standard.relay.metered.ca:443?transport=tcp",
+        username: "fe67734f65cabae0c1f0bf61",
+        credential: "AY3FDMwL9QjEIZ2R",
+      },
+
                   { urls: "stun:stun.l.google.com:19302" },
                   { urls: "stun:stun1.l.google.com:19302" },
                      { urls: "stun:global.stun.twilio.com:3478" }
